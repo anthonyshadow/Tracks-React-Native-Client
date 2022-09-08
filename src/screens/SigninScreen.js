a30c08901e1d69981a74946a9e5ff15b2083e57c
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-09-08 12:15:13 -0400

    Set up sign in screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SigninScreen = () => {
   return (
-    <View>
-      <Text>SigninScreen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to You're Account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Don't have an account? Sign up instead."
+        routeName="Signup"
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SigninScreen;
